a38da08a7d5a0693446b0a5ad13e9a27c6c05a33
Kevin <Kevin@163.com>
2025-03-30 11:24:54 +0800

    2025-03-30 11:24:54

--- code/chapter-6/01_summarywriter.py
+++ code/chapter-6/01_summarywriter.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     # download dataset from
     # 链接：https://pan.baidu.com/s/1szfefHgGMeyh6IyfDggLzQ
     # 提取码：ruzz
-    path_img = r"F:\pytorch-tutorial-2nd\data\datasets\covid-19-dataset-3\imgs\ryct.2020200028.fig1a.jpeg"
+    path_img = r"/media/code/dataset/val2017/000000031248.jpg"
     img_opencv = cv2.imread(path_img)
     writer.add_image('img_opencv_HWC-shape:{}'.format(img_opencv.shape), img_opencv, 0, dataformats='HWC')
     writer.close()
